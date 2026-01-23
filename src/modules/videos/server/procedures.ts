91d60a0189b423bd06a9b5eeeb3196e81ebd02d3
박기순 <ks.park@sqisoft.com>
2026-01-23 17:32:59 +0900

    구독 시스템 (스키마 완성)

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -63,7 +63,6 @@ export const videosRouter = createTRPCRouter({
     .innerJoin(users, eq(videos.userId, users.id))
     .leftJoin(viewerReactions, eq(viewerReactions.videoId, videos.id))
     .where(eq(videos.id, input.id))
-    .limit(1);
     // .groupBy(
     //   videos.id,
     //   users.id,
