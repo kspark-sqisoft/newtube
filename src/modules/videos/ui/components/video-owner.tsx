dff915f75c7e7d5e8c21e0e6e21de65a11513678
박기순 <ks.park@sqisoft.com>
2026-01-28 15:14:25 +0900

    구독 기능 완료

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
 import { UserInfo } from "@/modules/users/ui/components/user-info";
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
 
 interface VideoOwnerProps {
     user: VideoGetOneOutput["user"];
@@ -12,7 +13,14 @@ interface VideoOwnerProps {
 }
 
 export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
-    const {userId:clerkUserId} = useAuth();
+    const {userId:clerkUserId, isLoaded} = useAuth();
+
+    const {isPending, onClick} = useSubscription({
+        userId:user.id,
+        isSubscribed:user.viewerSubscribed,
+        fromVideoId:videoId,
+    })
+    
     return (
         <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
             <Link href={`/users/${user.id}`}>
@@ -22,7 +30,7 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
                         < UserInfo name={user.name} size="lg" />
                         <span className="text-sm text-muted-foreground line-clamp-1">
                             {/* TODO: Add subscribers count */}
-                            {0} subscribers
+                            {user.subscriberCount} subscribers
                         </span>
                     </div>
                 </div>
@@ -39,9 +47,9 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
                 </Button>
             ):(
                 <SubscriptionButton
-                onClick={()=>{}}
-                disabled={false}
-                isSubscribed={false}
+                onClick={onClick}
+                disabled={isPending || !isLoaded}
+                isSubscribed={user.viewerSubscribed}
                 className="flex-none"
                 />
             )}
