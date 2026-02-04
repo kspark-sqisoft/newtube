3fc59059c386cf8fcd90eaa2855d016bfad353a4
박기순 <ks.park@sqisoft.com>
2026-02-04 17:36:54 +0900

    개선 사항

--- src/modules/search/ui/sections/result-section.tsx
+++ src/modules/search/ui/sections/result-section.tsx
@@ -3,47 +3,89 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { trpc } from "@/trpc/client";
-import { VideoRowCard, VideoRowCardSkeleton } from "@/modules/videos/ui/components/video-row-card";
-import { VideoGridCard, VideoGridCardSkeleton } from "@/modules/videos/ui/components/video_grid_card";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/video-row-card";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video_grid_card";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface ResultsSectionProps {
-    query: string | undefined;
-    categoryId: string | undefined;
+  query: string | undefined;
+  categoryId: string | undefined;
 }
 
-export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
-    const isMobile = useIsMobile();
-    const [results, resultsQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
-        {
-            query,
-            categoryId,
-            limit: DEFAULT_LIMIT,
-        }, {
-        getNextPageParam: (lastPage) => lastPage.nextCursor,
-    }
-    );
-    return (
-        <>
-            {isMobile ? (
-                <div className="flex flex-col gap-4">
-                    {results.pages.flatMap((page) => page.items).map((video) => (
-                        <VideoGridCard key={video.id} data={video} />
-                    ))}
-                </div>
-            ) : (
-                <div className="flex flex-col gap-4">
-                    {results.pages.flatMap((page) => page.items).map((video) => (
-                        <VideoRowCard key={video.id} data={video} />
-                    ))}
-                </div>
-            )}
-            <InfiniteScroll
-                isManual={!isMobile}
-                hasNextPage={resultsQuery.hasNextPage}
-                isFetchingNextPage={resultsQuery.isFetchingNextPage}
-                fetchNextPage={resultsQuery.fetchNextPage}
-            />
-        </>
-    );
+export const ResultsSection = (props: ResultsSectionProps) => {
+  return (
+    <Suspense
+      key={`${props.query}-${props.categoryId}`}
+      fallback={<ResultsSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <ResultsSectionSuspense {...props} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const ResultsSectionSkeleton = () => {
+  return (
+    <>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} />
+        ))}
+      </div>
+      {/* 모바일 */}
+      <div className="flex flex-col gap-4 p-4 gap-y-10 pt-6 md:hidden">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
+  const isMobile = useIsMobile();
+  const [results, resultsQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
+    {
+      query,
+      categoryId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
+  return (
+    <>
+      {/* 모바일: md 브레이크포인트 미만에서만 보임 */}
+      <div className="flex flex-col gap-4 p-4 gap-y-10 pt-6 md:hidden">
+        {results.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+      {/* 데스크톱: md 브레이크포인트 이상에서만 보임 */}
+      <div className="hidden flex-col gap-4 md:flex">
+        {results.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoRowCard key={video.id} data={video} />
+          ))}
+      </div>
+      <InfiniteScroll
+        isManual={!isMobile}
+        hasNextPage={resultsQuery.hasNextPage}
+        isFetchingNextPage={resultsQuery.isFetchingNextPage}
+        fetchNextPage={resultsQuery.fetchNextPage}
+      />
+    </>
+  );
 };
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,118 +1,136 @@
 import { db } from "@/db";
-import { subscriptions, users, videoReactions, videos, videoUpdateSchema, videoViews } from "@/db/schema";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import {
+  subscriptions,
+  users,
+  videoReactions,
+  videos,
+  videoUpdateSchema,
+  videoViews,
+} from "@/db/schema";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
 import { mux } from "@/lib/mux";
 import { TRPCError } from "@trpc/server";
 import { and, eq, getTableColumns, inArray, isNotNull } from "drizzle-orm";
-import {z} from "zod";
+import { z } from "zod";
 import { UTApi } from "uploadthing/server";
 import { workflow } from "@/lib/workflow";
 
 export const videosRouter = createTRPCRouter({
-
   getOne: baseProcedure
-  .input(z.object({ id: z.string().uuid() }))
-  .query(async({ input, ctx})=>{
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ input, ctx }) => {
+      const { clerkUserId } = ctx;
+      let userId;
+      const [user] = await db
+        .select()
+        .from(users)
+        .where(inArray(users.clerkId, clerkUserId ? [clerkUserId] : []));
 
-    const {clerkUserId} = ctx;
-    let userId;
-    const [user] = await db
-      .select()
-      .from(users)
-      .where( inArray(users.clerkId, clerkUserId ? [clerkUserId] : []));
+      if (user) {
+        userId = user.id;
+      }
 
-    if(user){
-      userId = user.id;
-    }
+      const viewerReactions = db.$with("viewer_reactions").as(
+        db
+          .select({
+            videoId: videoReactions.videoId,
+            type: videoReactions.type,
+          })
+          .from(videoReactions)
+          .where(inArray(videoReactions.userId, userId ? [userId] : [])),
+      );
 
-    const viewerReactions = db.$with("viewer_reactions").as(
-      db.select({
-        videoId: videoReactions.videoId,
-        type: videoReactions.type,
-      })
-      .from(videoReactions)
-      .where(inArray(videoReactions.userId, userId ? [userId] : []))
-    );
+      const viewerSubscriptions = db.$with("viewer_subscriptions").as(
+        db
+          .select()
+          .from(subscriptions)
+          .where(inArray(subscriptions.viewerId, userId ? [userId] : [])),
+      );
 
-    const viewerSubscriptions = db.$with("viewer_subscriptions").as(
-      db.select()
-      .from(subscriptions)
-      .where(inArray(subscriptions.viewerId, userId ? [userId] : []))
-    );
-
-    const [existingVideo] = await db
-    .with(viewerReactions, viewerSubscriptions)
-    .select({
-      ...getTableColumns(videos),
-      user: {
-        ...getTableColumns(users),
-        subscriberCount: db.$count(subscriptions, eq(subscriptions.creatorId, users.id)),
-        viewerSubscribed: isNotNull(viewerSubscriptions.viewerId).mapWith(Boolean),
-      },
-      viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
-      likeCount: db.$count(
-        videoReactions,
-        and(
-          eq(videoReactions.videoId, videos.id),
-          eq(videoReactions.type, "like")
+      const [existingVideo] = await db
+        .with(viewerReactions, viewerSubscriptions)
+        .select({
+          ...getTableColumns(videos),
+          user: {
+            ...getTableColumns(users),
+            subscriberCount: db.$count(
+              subscriptions,
+              eq(subscriptions.creatorId, users.id),
+            ),
+            viewerSubscribed: isNotNull(viewerSubscriptions.viewerId).mapWith(
+              Boolean,
+            ),
+          },
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like"),
+            ),
+          ),
+          dislikeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike"),
+            ),
+          ),
+          viewerReaction: viewerReactions.type,
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .leftJoin(viewerReactions, eq(viewerReactions.videoId, videos.id))
+        .leftJoin(
+          viewerSubscriptions,
+          eq(viewerSubscriptions.creatorId, users.id),
         )
-      ),
-      dislikeCount: db.$count(
-        videoReactions,
-        and(
-          eq(videoReactions.videoId, videos.id),
-          eq(videoReactions.type, "dislike")
-        )
-      ),
-      viewerReaction: viewerReactions.type,
-    })
-    .from(videos)
-    .innerJoin(users, eq(videos.userId, users.id))
-    .leftJoin(viewerReactions, eq(viewerReactions.videoId, videos.id))
-    .leftJoin(viewerSubscriptions, eq(viewerSubscriptions.creatorId, users.id))
-    .where(eq(videos.id, input.id))
-    // .groupBy(
-    //   videos.id,
-    //   users.id,
-    //   viewerReactions.type,
-    // )
-    
+        .where(eq(videos.id, input.id));
+      // .groupBy(
+      //   videos.id,
+      //   users.id,
+      //   viewerReactions.type,
+      // )
 
-    if(!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
+      if (!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
 
-    return existingVideo;
-  }),
+      return existingVideo;
+    }),
 
   generateTitle: protectedProcedure
-  .input(z.object({ id: z.string().uuid() }))
-    .mutation(async({ctx, input})=>{
-      const {id:userId} = ctx.user;
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
       const { workflowRunId } = await workflow.trigger({
-        url:`${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
-        body:{userId, videoId:input.id}
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+        body: { userId, videoId: input.id },
       });
       return workflowRunId;
     }),
 
-    generateDescription: protectedProcedure
+  generateDescription: protectedProcedure
     .input(z.object({ id: z.string().uuid() }))
-      .mutation(async({ctx, input})=>{
-        const {id:userId} = ctx.user;
-        const { workflowRunId } = await workflow.trigger({
-          url:`${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/description`,
-          body:{userId, videoId:input.id}
-        });
-        return workflowRunId;
-      }),
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/description`,
+        body: { userId, videoId: input.id },
+      });
+      return workflowRunId;
+    }),
 
   generateThumbnail: protectedProcedure
-  .input(z.object({ id: z.string().uuid(), prompt: z.string().min(10) }))
-    .mutation(async({ctx, input})=>{
-      const {id:userId} = ctx.user;
+    .input(z.object({ id: z.string().uuid(), prompt: z.string().min(10) }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
 
       const { workflowRunId } = await workflow.trigger({
-        url:`${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/thumbnail`,
-        body:{userId, videoId:input.id, prompt:input.prompt}
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/thumbnail`,
+        body: { userId, videoId: input.id, prompt: input.prompt },
       });
       return workflowRunId;
     }),
@@ -124,21 +142,22 @@ export const videosRouter = createTRPCRouter({
       const [existingVideo] = await db
         .select()
         .from(videos)
-        .where(and(
-          eq(videos.id, input.id),
-          eq(videos.userId, userId)
-        ));
+        .where(and(eq(videos.id, input.id), eq(videos.userId, userId)));
 
-      if(!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
+      if (!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
 
-      if(!existingVideo.muxUploadId) throw new TRPCError({ code: "BAD_REQUEST" });
+      if (!existingVideo.muxUploadId)
+        throw new TRPCError({ code: "BAD_REQUEST" });
 
-      const upload = await mux.video.uploads.retrieve(existingVideo.muxUploadId);
-      if(!upload || !upload.asset_id) throw new TRPCError({ code: "BAD_REQUEST" });
+      const upload = await mux.video.uploads.retrieve(
+        existingVideo.muxUploadId,
+      );
+      if (!upload || !upload.asset_id)
+        throw new TRPCError({ code: "BAD_REQUEST" });
 
       const asset = await mux.video.assets.retrieve(upload.asset_id);
 
-      if(!asset){
+      if (!asset) {
         throw new TRPCError({ code: "BAD_REQUEST" });
       }
 
@@ -168,33 +187,29 @@ export const videosRouter = createTRPCRouter({
       const [existingVideo] = await db
         .select()
         .from(videos)
-        .where(and(
-          eq(videos.id, input.id),
-          eq(videos.userId, userId)
-        ))
-      if(!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
+        .where(and(eq(videos.id, input.id), eq(videos.userId, userId)));
+      if (!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
 
-      
-      if(existingVideo.thumbnailKey) {
+      if (existingVideo.thumbnailKey) {
         const utapi = new UTApi();
         await utapi.deleteFiles(existingVideo.thumbnailKey);
-        await db.update(videos).set({thumbnailKey:null, thumbnailUrl:null}).where(and(
-          eq(videos.id, input.id),
-          eq(videos.userId, userId)
-        ));
+        await db
+          .update(videos)
+          .set({ thumbnailKey: null, thumbnailUrl: null })
+          .where(and(eq(videos.id, input.id), eq(videos.userId, userId)));
       }
 
+      if (!existingVideo.muxPlaybackId)
+        throw new TRPCError({ code: "BAD_REQUEST" });
 
-      if(!existingVideo.muxPlaybackId) throw new TRPCError({ code: "BAD_REQUEST" });
-
-      
       const utapi = new UTApi();
       const tempThumbnailUrl = `https://image.mux.com/${existingVideo.muxPlaybackId}/thumbnail.jpg`;
-      const uploadedThumbnail = await utapi.uploadFilesFromUrl(tempThumbnailUrl);
-      if(!uploadedThumbnail.data) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+      const uploadedThumbnail =
+        await utapi.uploadFilesFromUrl(tempThumbnailUrl);
+      if (!uploadedThumbnail.data)
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
 
-      const {key:thumbnailKey, url:thumbnailUrl} = uploadedThumbnail.data;
-
+      const { key: thumbnailKey, url: thumbnailUrl } = uploadedThumbnail.data;
 
       const [updatedVideo] = await db
         .update(videos)
@@ -202,58 +217,56 @@ export const videosRouter = createTRPCRouter({
           thumbnailUrl,
           thumbnailKey,
         })
-        .where(and(
-          eq(videos.id, input.id),
-          eq(videos.userId, userId)
-        ))
+        .where(and(eq(videos.id, input.id), eq(videos.userId, userId)))
         .returning();
-      
+
       return updatedVideo;
     }),
 
-  remove: protectedProcedure.input(z.object({ id: z.string().uuid() })).mutation(async ({ ctx, input }) => {
-    const { id: userId } = ctx.user;
+  remove: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
 
-    const [removeVideo] = await db
-      .delete(videos)
-      .where(and(
-        eq(videos.id, input.id), 
-        eq(videos.userId, userId)
-      ))
-      .returning();
+      const [removeVideo] = await db
+        .delete(videos)
+        .where(and(eq(videos.id, input.id), eq(videos.userId, userId)))
+        .returning();
 
-    if (!removeVideo) {
-      throw new TRPCError({ code: "NOT_FOUND" });
-    }
+      if (!removeVideo) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
 
-    return removeVideo;
-  }),
+      return removeVideo;
+    }),
 
-  update: protectedProcedure.input(videoUpdateSchema).mutation(async ({ ctx, input }) => {
-    const { id: userId } = ctx.user;
+  update: protectedProcedure
+    .input(videoUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
 
-    if (!input.id) {
-      throw new TRPCError({ code: "BAD_REQUEST" });
-    }
+      if (!input.id) {
+        throw new TRPCError({ code: "BAD_REQUEST" });
+      }
 
-    const [updatedVideo] = await db
-      .update(videos)
-      .set({
-        title: input.title,
-        description: input.description,
-        categoryId: input.categoryId,
-        visibility: input.visibility,
-        updatedAt: new Date(),
-      })
-      .where(and(eq(videos.id, input.id), eq(videos.userId, userId)))
-      .returning();
+      const [updatedVideo] = await db
+        .update(videos)
+        .set({
+          title: input.title,
+          description: input.description,
+          categoryId: input.categoryId,
+          visibility: input.visibility,
+          updatedAt: new Date(),
+        })
+        .where(and(eq(videos.id, input.id), eq(videos.userId, userId)))
+        .returning();
 
-    if (!updatedVideo) {
-      throw new TRPCError({ code: "NOT_FOUND" });
-    }
+      if (!updatedVideo) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
 
-    return updatedVideo;
-  }),
+      return updatedVideo;
+    }),
 
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
@@ -262,16 +275,16 @@ export const videosRouter = createTRPCRouter({
       new_asset_settings: {
         passthrough: userId,
         playback_policy: ["public"],
-        input:[
+        input: [
           {
-            generated_subtitles:[
+            generated_subtitles: [
               {
                 language_code: "en",
                 name: "English",
-              }
-            ]
-          }
-        ]
+              },
+            ],
+          },
+        ],
       },
       cors_origin: "*", //TODO: In production, set to your url
     });
